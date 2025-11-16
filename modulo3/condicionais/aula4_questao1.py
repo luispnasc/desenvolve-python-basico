5f6a25b0aa12e39c32aec4bfd750f2ad65b90937
luispnasc <luispnasc@gmail.com>
2025-11-16 16:49:15 -0300

    Add files via upload

---
+++ modulo3/condicionais/aula4_questao1.py
@@ -0,0 +1,6 @@
+n1, n2 = int(input()), int(input())
+
+if (n1 + n2) % 2 == 0:
+    print("É par")
+else:
+    print ("É ímpar")    
---
+++ modulo3/condicionais/aula4_questao2.py
@@ -0,0 +1,14 @@
+avaliacao = int(input("Digite a avaliação do filme: "))
+
+if avaliacao == 5:
+    print("Excelente!")
+if avaliacao == 4:
+    print("Muito bom!")
+if avaliacao == 3:
+    print("Bom!")        
+if avaliacao == 2:
+    print("Regular!")
+if avaliacao == 1:
+    print("Ruim!")  
+print("Fim!")      
+ 
